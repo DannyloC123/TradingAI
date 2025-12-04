96773c24780fe1254c1cf1dae9acecb3d80ed497
Dannylo Correia <DannyloCorreia2006@gmail.com>
2025-12-04 15:40:49 -0500

    Update

--- main.py
+++ main.py
@@ -7,6 +7,26 @@ from exchange_sdk.client import GatewayConfig, MarketDataConfig
 # Load token from tokens.env
 load_dotenv("tokens.env")
 
+class XYZ:
+    type = 'Stock'
+    tickSize = 0.01
+    symbolID = 1
+
+class ETF:
+    type = 'Fund'
+    tickSize = 0.01
+    symbolID = 2
+    
+class ABC:
+    type = 'Stock'
+    tickSize = 0.01
+    symbolID = 3
+
+class DEF:
+    type = 'Stock'
+    tickSize = 0.01
+    symbolID = 4
+
 async def main():
     client = ExchangeClient(
         team_token=os.getenv("TEAM_TOKEN"),
@@ -16,6 +36,18 @@ async def main():
 
     await client.connect()
     print("Connected successfully!")
+
+    # Needed to Submit Order
+    order_id = await client.send_new_async(
+        client_id=1,                 # internal ID you choose
+        symbol_id=XYZ.symbolID,      # choose stock using your class
+        side=0,                      # 0 = BUY
+        price_ticks=10000,           # $100.00
+        quantity=10                  # buy 10 shares
+    )
+    
+    print("Order sent! Order ID:", order_id)
+
     await client.close()
 
 asyncio.run(main())
